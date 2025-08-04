ec2f06c3b889d6670799372867bf5dab0c5e17ba
mateuscamargo <mateuscamargolima@gmail.com>
2025-08-03 21:43:14 -0300

    Add postagens do blog

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cadastro from "./cadastro/Cadastro";
+import FormPostagem from "./components/postagens/formpostagem/FormPostagem";
+import ListaPostagens from "./components/postagens/listapostagens/ListaPostagens";
+import DeletarTema from "./components/temas/deletartema/DeletarTemas";
 import FormTema from "./components/temas/formtema/FormTemas";
 import ListaTemas from "./components/temas/listatemas/ListaTemas";
 import { UserProvider } from "./contexts/UserContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
-import DeletarTema from "./components/temas/deletartema/DeletarTemas";
 
 function App() {
 
@@ -21,6 +23,9 @@ function App() {
           <Route path="/cadastrartema" element={<FormTema />} />
           <Route path="/editartema/:id" element={<FormTema />} />
           <Route path="/deletartema/:id" element={<DeletarTema />} />
+          <Route path="/postagens" element={<ListaPostagens />} />
+          <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+          <Route path="/editarpostagem/:id" element={<FormPostagem />} />
         </Routes>
       </BrowserRouter>
     </UserProvider>
