7ec8551e6c43d7a1c5e0ec34c4fb652fd8aeb636
mateuscamargo <mateuscamargolima@gmail.com>
2025-08-03 22:09:26 -0300

    Add cadastro do perfil de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Cadastro from "./cadastro/Cadastro";
-import FormPostagem from "./components/postagens/formpostagem/FormPostagem";
-import ListaPostagens from "./components/postagens/listapostagens/ListaPostagens";
-import DeletarTema from "./components/temas/deletartema/DeletarTemas";
-import FormTema from "./components/temas/formtema/FormTemas";
-import ListaTemas from "./components/temas/listatemas/ListaTemas";
-import { UserProvider } from "./contexts/UserContext";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
+import Cadastro from './cadastro/Cadastro'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import DeletarTema from './components/temas/deletartema/DeletarTemas'
+import FormTema from './components/temas/formtema/FormTemas'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import { AuthProvider } from './contexts/AuthContext'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
-
   return (
-    <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/temas" element={<ListaTemas />} />
-          <Route path="/cadastrartema" element={<FormTema />} />
-          <Route path="/editartema/:id" element={<FormTema />} />
-          <Route path="/deletartema/:id" element={<DeletarTema />} />
-          <Route path="/postagens" element={<ListaPostagens />} />
-          <Route path="/cadastrarpostagem" element={<FormPostagem />} />
-          <Route path="/editarpostagem/:id" element={<FormPostagem />} />
-        </Routes>
-      </BrowserRouter>
-    </UserProvider>
-  );
+    <>
+      <AuthProvider>
+        <ToastContainer/>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  )
 }
 
-export default App;
+export default App
